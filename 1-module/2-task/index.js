6887f32f47957fb7a67753344a8deae7571ae3fc
Roman Bogomolov <individ114@mail.ru>
2022-07-16 13:55:41 +0300

    Дописал функцию валидации имени пользователя

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,39 @@
 /**
- * Эту функцию трогать не нужно
+ * Эту функцию трогать не нужно`
  */
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
+/*
+  Эту функцию нужно поменять так,
+  чтобы функция sayHello работала корректно
+  @param {string | null} name
+  @returns {boolean}
  */
 function isValid(name) {
-  // ваш код...
+  if (name && name.length >= 4) {
+    for (let i = 0; i < name.length; i += 1) {
+      if (name[i] === " ") {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }
 
+/**
+ * Эту функцию трогать не нужно
+ */
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
+
+sayHello();
