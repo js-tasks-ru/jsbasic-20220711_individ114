a7979167f8e430ac298ee3694d37176081b61720
Roman Bogomolov <individ114@mail.ru>
2022-07-20 22:51:01 +0300

    Выполнил все 3 задачи второго модуля

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,14 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    calculator.numOne = a;
+    calculator.numTwo = b;
+  },
+  sum() {
+    return calculator.numOne + calculator.numTwo;
+  },
+  mul() {
+    return calculator.numOne * calculator.numTwo;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
