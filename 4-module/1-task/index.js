0a1f484b29857b1fe3dbba2066a2da1719338e90
Roman Bogomolov <individ114@mail.ru>
2022-07-29 00:51:58 +0300

    4 modul all task completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement("ul");
+  list.id = "qwerty";
+  let friendsFilter = friends.map(
+    (elem) => `${elem.firstName} ${elem.lastName}`
+  );
+  for (let i = 0; i < friendsFilter.length; i += 1) {
+    let item = document.createElement("li");
+    item.innerHTML = friendsFilter[i];
+    list.append(item);
+  }
+  return list;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  for (let i of table.querySelectorAll("tbody > tr")) {
+    console.log(i);
+    console.log(i.querySelectorAll("td"));
+    let nullAvailable = 0;
+
+    for (let j of i.children) {
+      console.log(j);
+      if (j.innerHTML === "m") i.classList.add("male");
+      if (j.innerHTML === "f") i.classList.add("female");
+      if (j.innerHTML < 18) i.style = "text-decoration: line-through";
+      if (j.dataset.available === "true") {
+        i.classList.add("available");
+        continue;
+      }
+      if (j.dataset.available === "false") {
+        i.classList.add("unavailable");
+        continue;
+      }
+      nullAvailable += 1;
+      if (nullAvailable == i.children.length) i.hidden = true;
+    }
+  }
 }
