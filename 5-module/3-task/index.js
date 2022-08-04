e993b5a41e7ce5fc0ef09379f57650fe00782714
Roman Bogomolov <individ114@mail.ru>
2022-08-04 14:38:46 +0300

    5 modul all tasks completed

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let right = document.querySelector(".carousel__arrow_right");
+  let left = document.querySelector(".carousel__arrow_left");
+  let slides = document.querySelectorAll(".carousel__slide");
+  let carusel = document.querySelector(".carousel__inner");
+  let caruselWidth = getComputedStyle(
+    document.querySelector(".carousel__slide")
+  );
+  let transCount = 0;
+  left.style.display = "none";
+
+  right.addEventListener("click", () => {
+    transCount += -parseInt(caruselWidth.width);
+    if (transCount === transCount)
+      carusel.style.transform = `translateX(${transCount}px)`;
+
+    console.log(transCount);
+    if (transCount === -(parseInt(caruselWidth.width) * 3))
+      right.style.display = "none";
+    left.style.display = "";
+  });
+
+  left.addEventListener("click", () => {
+    transCount += parseInt(caruselWidth.width);
+    for (let slide of slides) {
+      if (transCount === transCount)
+        carusel.style.transform = `translateX(${transCount}px)`;
+    }
+
+    console.log(transCount);
+    if (transCount === 0) left.style.display = "none";
+    right.style.display = "";
+  });
 }
