b3c8b3727034f8062e2a47b307ad1e82686fafdf
Roman Bogomolov <individ114@mail.ru>
2022-08-12 00:57:38 +0300

    6 modul all tasks is ready

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,97 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.mans = rows;
+    this.tableHTML = document.createElement("table");
+    this.elem;
+    this.removed();
+    this.div();
+  }
+
+  div() {
+    let html = `<thead>
+    <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+    </tr>
+</thead>
+<tbody>
+        ${this.mans
+          .map(
+            (key) => `<tr data-component="tr">
+          <td>${key.name}</td>
+          <td>${key.age}</td>
+          <td>${key.salary}</td>
+          <td>${key.city}</td>
+          <td><button data-action="remove">X</button></td>
+          </tr>`
+          )
+          .join("")}
+</tbody>`;
+    this.tableHTML.innerHTML = html;
+  }
+
+  removed() {
+    this.tableHTML.addEventListener("click", (event) => {
+      let target = event.target;
+      if (target.dataset.action !== "remove") {
+        return;
+      }
+      let tr = target.closest("[data-component='tr']");
+      if (!tr) {
+        return;
+      }
+      tr.remove();
+    });
+  }
+
+  get elem() {
+    return this.tableHTML;
   }
 }
+
+///////////////////////////////  Ниже вариант другого участника курса.
+
+// export default class UserTable {
+//   constructor(rows) {
+//     this.mans = rows;
+//     this.elem = document.createElement("table");
+//     this.makeHTML();
+//   }
+
+//   makeHTML() {
+//     let cell = `<thead>
+//     <tr>
+//         <th>Имя</th>
+//         <th>Возраст</th>
+//         <th>Зарплата</th>
+//         <th>Город</th>
+//         <th></th>
+//     </tr>
+// </thead>
+// <tbody>
+//         ${this.mans
+//           .map(
+//             (key) => `<tr data-component="tr">
+//           <td>${key.name}</td>
+//           <td>${key.age}</td>
+//           <td>${key.salary}</td>
+//           <td>${key.city}</td>
+//           <td><button data-action="remove">X</button></td>
+//           </tr>`
+//           )
+//           .join("")}
+// </tbody>`;
+//     this.elem.innerHTML = cell;
+//     for (let btnlist of this.elem.querySelectorAll("button"))
+//       btnlist.addEventListener("click", this);
+//   }
+
+//   handleEvent(event) {
+//     let row = event.target.parentElement.parentElement;
+//     row.remove();
+//   }
+// }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,52 @@
+import createElement from "../../assets/lib/create-element.js";
 export default class ProductCard {
   constructor(product) {
+    this.name = product.name;
+    this.price = product.price;
+    this.category = product.category;
+    this.image = product.image;
+    this.id = product.id;
+    this.elem;
+    this.html();
+    this.click();
+  }
+
+  html() {
+    let table = createElement(`<div class="card">
+    <div class="card__top">
+        <img src="/assets/images/products/${
+          this.image
+        }" class="card__image" alt="product">
+        <span class="card__price">€${this.price.toFixed(2)}</span>
+    </div>
+    <div class="card__body">
+        <div class="card__title">${this.name}</div>
+        <button type="button" class="card__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+    </div>
+</div>`);
+    this.card = table;
+  }
+
+  button() {
+    return this.card.querySelector(".card__button");
+  }
+
+  click() {
+    this.button().addEventListener("click", this.eventPlus);
+  }
+
+  eventPlus = () => {
+    let userEvent = new CustomEvent("product-add", {
+      detail: this.id,
+      bubbles: true,
+    });
+    console.log(this);
+    this.card.dispatchEvent(userEvent);
+  };
+
+  get elem() {
+    return this.card;
   }
 }
